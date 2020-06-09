904083571fedfc04b79b4e90a363ff1d8b376281
liaoyuting2020 <2337468643@qq.com>
2020-06-09 17:27:29 +0800

    提交到本次仓库

---
+++ Servers2/src/src/UserController.java
@@ -0,0 +1,40 @@
+package src;
+
+public class UserController {
+     private int id;
+     private String Username;
+     private String password;
+     private int sex;
+     private String love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+    
+}
